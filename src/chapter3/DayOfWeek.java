6354d26be101af50a667cc39f1857cd9701b0117
knowpract <yuan_zju@outlook.com>
2019-10-27 23:25:56 +0800

    习题 3.21

--- src/chapter3/DayOfWeek.java
+++ src/chapter3/DayOfWeek.java
@@ -8,7 +8,8 @@ public class DayOfWeek {
         System.out.print("Enter year: (e.g., 2012): ");
         int year = input.nextInt();
         System.out.print("Enter month: 1-12: ");
-        int month = input.nextInt();
+        int month = input
+                .nextInt();
         System.out.print("Enter the day of the month: 1-31: ");
         int day = input.nextInt();
         if (month == 1 || month == 2) {
